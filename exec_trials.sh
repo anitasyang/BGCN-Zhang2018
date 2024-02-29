c6583991cf103e85c2eafc4449a09754d2ad2f87
anita <anitayang215@gmail.com>
2024-02-29 15:59:55 +0900

    sh file to conduct all runs across all datasets

--- exec_trials.sh
+++ exec_trials.sh
@@ -1,13 +1,14 @@
-dataset=$1
-no_dropout=$2
+#! /bin/bash
 
-for ((i=1; i<=50; i++))
+list=("cora" "citeseer" "pubmed")
+
+for dataset in "${list[@]}"
 do
-    if [ "$no_dropout" == "no_dropout" ]
-    then
-        echo "Running with no dropout"
+    echo "Running $dataset"
+    for ((i=1; i<=50; i++))
+    do
+        echo "num iter: $i"
         python3 BGCN_main.py --dataset $dataset --no_dropout
-    else
         python3 BGCN_main.py --dataset $dataset
-    fi
+    done
 done
